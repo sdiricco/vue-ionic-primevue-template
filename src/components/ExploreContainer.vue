242306c0370266f21b0472e2d3ccd05ac0eab4ce
Simone <sdiricco.91@gmail.com>
2024-03-02 22:13:30 +0100

    add pinia, iconify

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,14 +1,40 @@
 <template>
   <div class="p-6">
-    <div class="flex align-items-center justify-content-center">
+    <div class="flex align-items-center justify-content-center pb-6">
       <span class="mr-2">Toggle theme</span>
       <InputSwitch v-model:model-value="isDark" />
+    </div>
+
+    <div class="flex gap-4 align-items-center justify-content-center">
+      <Button aria-label="Filter" @click="decrement">
+        <template #icon>
+          <Icon icon="ic:baseline-minus" />
+        </template>
+      </Button>
+      <div>
+        Count
+        <span>
+          {{ count }}
+        </span>
+      </div>
+
+      <Button aria-label="Filter" @click="increment">
+        <template #icon>
+          <Icon icon="ic:baseline-plus" />
+        </template>
+      </Button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useDark, useToggle } from "@vueuse/core";
+import { useDark } from "@vueuse/core";
+import { useCounterStore } from "@/store/main";
+import { storeToRefs } from "pinia";
+
+const counter = useCounterStore();
+const { count } = storeToRefs(counter);
+const { increment, decrement } = counter;
 
 const isDark = useDark({
   selector: "body",
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import { createPinia } from 'pinia'
 import PrimeVue from "primevue/config";
 import IonicImporter from "./plugins/ionic";
 import PrimeVueImporter from "./plugins/primevue";
+import { Icon } from '@iconify/vue';
+
+const pinia = createPinia()
 
 const app = createApp(App)
+  .use(pinia)
   .use(PrimeVue, { ripple: true })
   .use(IonicImporter)
   .use(router)
   .use(PrimeVueImporter);
+
+app.component("Icon", Icon)
   
 router.isReady().then(() => {
   app.mount('#app');
